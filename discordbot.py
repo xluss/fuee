700028bbf174c70afd69896f375dd2359af6e123
shelcis <51266644+shelcis@users.noreply.github.com>
2019-06-21 00:55:20 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -19,7 +19,7 @@ dateTimeList = [
 
 
 # 自分のBotのアクセストークンに置き換えてください
-TOKEN = 'NTg0Mzk2NTUwOTIzNDg1MjAw.XQo0yA.gtLrSkLe3eL7p8e2_aq4b-Higg8'
+TOKEN = 'NTg0Mzk2NTUwOTIzNDg1MjAw.XQuG4g.KoOFdHVg7R1gS4RIbEcftJzb9ek'
 
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
@@ -172,4 +172,4 @@ async def loop():
 loop.start()
 
 # Botの起動とDiscordサーバーへの接続
-client.run('NTg0Mzk2NTUwOTIzNDg1MjAw.XQo0yA.gtLrSkLe3eL7p8e2_aq4b-Higg8')
+client.run('NTg0Mzk2NTUwOTIzNDg1MjAw.XQuG4g.KoOFdHVg7R1gS4RIbEcftJzb9ek')
